e66f1079c57fb339f272a1af9cbdb6f2ae4ee5ee
CyprianS <szwajka.cyprian@gmail.com>
2017-08-25 11:20:19 +0200

    'TodoItem' and 'TodoList' classes implemented

--- TodoItem.java
+++ TodoItem.java
@@ -1,7 +1,14 @@
-/**
- * Class representing a single todo item.
- * 
- */
 public class TodoItem{
 
+    public String content;
+    public boolean isCompleted;
+
+    public TodoItem(String content, boolean isCompleted) {
+        this.content = content;
+        this.isCompleted = isCompleted;
+    }
+
+    public void markAsCompleated() {
+        this.completed = true;
+    }
 }
--- TodoList.java
+++ TodoList.java
@@ -1,7 +1,20 @@
-/**
- * Class representing a todo list.
- * TodoList object should aggregate TodoItem objects.
- */
 public class TodoList{
 
+    public ArrayList<TodoItem> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<TodoItem>();
+    }
+
+    public void add(TodoItem task) {
+        this.tasks.add(task);
+    }
+
+    public void archive_all() {
+        ArrayList<TodoItem> undoneTasks = new ArrayList<TodoItem>();
+
+        for (TodoItem task : this.tasks) {
+            if (task.isCompleted == false) { undoneTasks.add(task) }
+        }
+    }
 }
