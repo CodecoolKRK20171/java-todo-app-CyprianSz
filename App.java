59aec57498e7c02bb05fe0fa977d57432be580ab
CyprianS <szwajka.cyprian@gmail.com>
2017-08-25 11:57:46 +0200

    Main App implemented

--- App.java
+++ App.java
@@ -1,9 +1,24 @@
-/**
- * Class representing the TodoApp application.
- * It is the main entry point for this program.
- */
 public class App{
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller();
+        TodoList toDoList = new TodoList();
+        boolean proceed = true;
 
-  }
+        while (proceed) {
+            String choice = view.takeUserChoice();
+
+            if (choice.equals("list")) {
+                view.printAllTasks(toDoList);
+            } else if (choice.equals("add")) {
+                controller.addNewTask(toDoList);
+            } else if (choice.equals("mark")) {
+                controller.markTaskAsCompleated(toDoList);
+            } else if (choice.equals("archive")) {
+                controller.archiveAllCompleatedTasks(toDoList);
+            } else if (choice.equals("exit")) {
+                proceed = false;
+            }
+        }
+    }
 }
